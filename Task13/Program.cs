4315273cdfb02a86c5b70ad138422890045a1ae1
Safiia <sonymamedova@gmail.com>
2022-06-26 09:39:44 +0300

    add Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,30 +6,31 @@
 
 void ThirdNum(int a)
 {
-if (Math.Abs(a) < 100)
-{
-    Console.WriteLine(a + " -> Третьей цифры нет");
-}
-else (Math.Abs(a) > 1000);
-{
-   a = a / 10;
- }
-int n = Math.Abs(a) % 10;
-Console.WriteLine(n);
+    if (Math.Abs(a) < 100)
+    {
+        Console.WriteLine(a + " -> Третьей цифры нет");
+    }
+    while (Math.Abs(a) > 1000)
+    {
+        a = a / 10;
+    }
+    int b = Math.Abs(a / 10) % 10;
+    Console.WriteLine(b);
 }
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 ThirdNum(num);
 
-
 // Console.Write("Введите число: ");
-// int anyNumber = Convert.ToInt32(Console.ReadLine());
-// string anyNumberText = Convert.ToString(anyNumber);
-// if (anyNumberText.Length > 2)
+// int Number = Convert.ToInt32(Console.ReadLine());
+// string NumberText = Convert.ToString(Number);
+// if (NumberText.Length > 2)
 // {
-//   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+//   Console.WriteLine("третья цифра -> " + NumberText[2]);
 // }
 // else 
 // {
 //   Console.WriteLine("-> третьей цифры нет");
 // }
+
+
